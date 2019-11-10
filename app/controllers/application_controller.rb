1a39d69cb9212fb34d2f8edfcc61fc5f3a42f7c8
khozi <khozaima.mahmoodi@gmail.com>
2019-11-10 18:16:44 +0330

    Override authenticate_user! method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-    include Pundit
-    protect_from_forgery
+  include Pundit
+  before_action :authenticate_user!
+  protect_from_forgery prepend: true
+
+private
+
+  def authenticate_user!
+    return super if user_signed_in?
+
+    render json: { message: 'You are not loged in! Please try agin' }
+  end
 end
