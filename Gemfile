759dc0ba3a0263f8f24b8c51b1a147a0775049ae
khozi <khozaima.mahmoodi@gmail.com>
2019-11-13 16:03:27 +0330

    Add shouda matchers gem

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   gem 'chromedriver-helper'
+  gem 'shoulda-matchers'
 end
 
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -88,4 +88,13 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::IntegrationHelpers, type: :request
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
 end
