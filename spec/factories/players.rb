4f5b5bd6211d62464b4a589600535b14ebac1aa5
khozi <khozaima.mahmoodi@gmail.com>
2019-11-28 11:49:36 +0330

    Edit player factory

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -2,25 +2,25 @@
 #
 # Table name: players
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  lastname   :string
-#  post       :string
-#  age        :integer
-#  image      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :integer
-#  team_id    :integer
+#  id          :integer          not null, primary key
+#  name        :string
+#  lastname    :string
+#  player_post :string
+#  age         :integer
+#  image       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#  team_id     :integer
 #
 
 FactoryGirl.define do
   factory :player do
     name { FFaker::Name.name }
     lastname { FFaker::Name.last_name }
-    post { FFaker::Name.post }
+    player_post { FFaker::Name.name }
     age { Faker::Number.age(digits: 3) }
-    image { FFaker::Image.image }
+    image { FFaker::Avatar.image }
 
     association :team, factory: :team
     association :user, factory: :user
