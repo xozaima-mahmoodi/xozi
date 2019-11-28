06aa6ff1299e392515b8458e654051faed6cdd03
khozi <khozaima.mahmoodi@gmail.com>
2019-11-28 11:55:40 +0330

    Complete team controller

--- app/controllers/api/mendil/team_controller.rb
+++ app/controllers/api/mendil/team_controller.rb
@@ -1,28 +1,53 @@
 module Api
   module Mendil
-    class UsersController < ApplicationController
-      def index
-        authorize (User)
 
-        users = User.all
-        render jsonapi: users
+    class TeamController < ApplicationController
+      before_action :find_team, only: [:show, :update, :destroy]
+
+      def index
+        team = Team.all
+        render jsonapi: team
       end
 
       def show
-        
+        render jsonapi: @team
       end
 
       def create
-        
+        team = Team.new(team_params)
+
+        if team.save
+          render jsonapi: team
+        else
+          render jsonapi: team.errors
+        end  
       end
 
       def update
-        
+        if @team.update(team_params)
+          render jsonapi: @team
+        else
+          render jsonapi: @team.errors 
+        end  
       end
 
       def destroy
-        
+        @team.destroy
+
+        flash[:notice] = "Team Removed"
+        redirect_to team_path
       end
+
+
+
+      private
+        def find_team
+          @team = Team.find(params[:id])
+        end
+
+        def team_params
+          params.require(:team).permit(:name)
+        end
     end
-  end
+  end      
 end
