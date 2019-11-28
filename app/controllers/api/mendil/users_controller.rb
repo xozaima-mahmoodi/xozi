1049b214b547480e85a31a1a9380ab0106e658a4
khozi <khozaima.mahmoodi@gmail.com>
2019-11-28 11:46:25 +0330

    Complete user controller

--- app/controllers/api/mendil/users_controller.rb
+++ app/controllers/api/mendil/users_controller.rb
@@ -3,26 +3,53 @@ module Api
     class UsersController < ApplicationController
       def index
         authorize (User)
-
         users = User.all
         render jsonapi: users
       end
 
       def show
-        
+        render jsonapi: @user
       end
 
       def create
-        
+        authorize(User)
+        user = User.new(user_params)
+        user.customer_id = current_user.id
+
+        if user.save
+          render jsonapi: user, status: :created
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
       end
 
       def update
-        
+        user = User.find(params[:id])
+        if user.update(user_params)
+          render jsonapi: user
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
       end
 
       def destroy
-        
+        @user.destroy
       end
+
+
+      private
+
+        def set_user
+        @user = User.find(params[:id])
+        authorize(@user)
+        end
+
+        
+        def user_params
+          params.require(:user).permit(
+            :first_name, :last_name, :email, :role
+          )
+        end
     end
   end
 end
