1fe649ccf5fd77961fa42e919ad7bafdaf24fdee
khozi <khozaima.mahmoodi@gmail.com>
2019-11-28 11:55:07 +0330

    Complete player controller

--- app/controllers/api/mendil/player_controller.rb
+++ app/controllers/api/mendil/player_controller.rb
@@ -1,28 +1,53 @@
 module Api
   module Mendil
-    class UsersController < ApplicationController
-      def index
-        authorize (User)
 
-        users = User.all
-        render jsonapi: users
+    class PlayerController < ApplicationController
+      before_action :find_player, only: [:show, :update, :destroy]
+
+      def index
+        player = Player.all
+        render jsonapi: player
       end
 
       def show
-        
+        render jsonapi: @player
       end
 
       def create
-        
+        player = Player.new(player_params)
+
+        if player.save
+          render jsonapi: player
+        else
+          render jsonapi: player.errors
+        end  
       end
 
       def update
-        
+        if @player.update(player_params)
+          render jsonapi: @player
+        else
+          render jsonapi: @player.errors 
+        end  
       end
 
       def destroy
-        
+        @player.destroy
+
+        flash[:notice] = "Player Removed"
+        redirect_to player_path
       end
+
+
+
+      private
+        def find_player
+          @player = Player.find(params[:id])
+        end
+
+        def player_params
+          params.require(:player).permit(:name)
+        end
     end
-  end
+  end      
 end
