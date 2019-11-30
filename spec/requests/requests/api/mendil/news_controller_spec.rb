f1d4cd18bdac3183aeabf19c3c11bbd74a9a90ab
khozi <khozaima.mahmoodi@gmail.com>
2019-11-30 12:10:54 +0330

    Rspec for create in news controller

--- spec/requests/requests/api/mendil/news_controller_spec.rb
+++ spec/requests/requests/api/mendil/news_controller_spec.rb
@@ -27,41 +27,25 @@ RSpec.describe 'News Controller', type: :request do
     end
   end
 
-  # context 'post news#create' do
-  #   it 'should create a news ' do
-  #      user = create :user, email:'test@test.com', password:'123456Aa!'
-  #      team = create :team, user_id: user.id
-  #      player = create :player, user_id: user.id, team_id: team.id
-  #      news = create :news, user_id: user.id, team_id: team.id, player_id: player.id
-  #       valid_news_params = {
-  #         news: {
-  #           title:'title',
-  #           description:'cccccc'
-  #         }
-  #       }
-
-  #      sign_in( user )
-
-  #      expect{ post "/api/mendil/news", params:valid_news_params }.to change(News, :count).by(1)
-  #   end
-  # end
-
-  context 'Post news#create' do
-    it 'should create a news and return 200 response' do
-      user = create :user, email: 'test@test.com', password: '123456Aa!'
-      team = create :team, user_id: user.id
-      player = create :player, user_id: user.id, team_id: team.id
-      news = create :news, user_id: user.id, team_id: team.id, player_id: player.id
-      
-      news_params = {
-        news: {
-          title:'title',
-          description:'cccccc'
+  context 'post news#create' do
+    it 'should create a news ' do
+       user = create :user, email:'test@test.com', password:'123456Aa!'
+       team = create :team, user_id: user.id
+       player = create :player, user_id: user.id, team_id: team.id
+        
+       valid_attributes = {
+          news: {
+            title:'title',
+            description:'cccccc',
+            user_id: user.id,
+            team_id: team.id,
+            player_id: player.id
+          }
         }
-      }
 
-      sign_in( user )
-      expect { post "/api/mendil/news", params: news_params }.to change(News, :count).by(1)
+       sign_in( user )
+
+       expect{ post "/api/mendil/news", params:valid_attributes }.to change(News, :count).by(1)
     end
   end
 
