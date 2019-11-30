50935a12b562e44e190a337f009309ec83d391d0
khozi <khozaima.mahmoodi@gmail.com>
2019-11-30 18:08:39 +0330

    Rspec for show in player controller

--- spec/requests/requests/api/mendil/player_controller_spec.rb
+++ spec/requests/requests/api/mendil/player_controller_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe 'Player Controller', type: :request do
       expect(response).to be_successful
     end
   end
+
+  context 'GET player#show' do
+    it 'should return 200 response' do
+      user = create :user, email: 'test@test.com', password: '123456Aa!'
+      team = create :team, user_id: user.id
+      player = create :player, first_name:'xozi', last_name:'mah', player_post:'forward', user_id: user.id, team_id: team.id
+
+      sign_in( user )
+      
+      get '/api/mendil/player'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+
 end
