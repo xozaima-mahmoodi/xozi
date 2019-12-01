f56c2bf396e598a56f44aaba75800f062516f435
khozi <khozaima.mahmoodi@gmail.com>
2019-12-01 10:46:33 +0330

    Rspec for show in team controller

--- spec/requests/requests/api/mendil/team_controller_spec.rb
+++ spec/requests/requests/api/mendil/team_controller_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe 'Team Controller', type: :request do
       expect(response).to be_successful
     end
   end
+
+  context 'GET team#show' do
+    it 'should return 200 response' do
+      user = create :user, email: 'test@test.com', password: '123456Aa!'
+      team = create :team, name:'perspolis', description:'666666', user_id: user.id
+
+      sign_in( user )
+      
+      get '/api/mendil/team'
+      expect(response).to have_http_status(200)
+    end
+  end
 end
