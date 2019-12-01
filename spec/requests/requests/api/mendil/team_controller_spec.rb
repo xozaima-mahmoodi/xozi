baba42d9d60fee9011d453c6fb01cc6c3405199c
khozi <khozaima.mahmoodi@gmail.com>
2019-12-01 10:41:29 +0330

    Rspec for index in team controller

--- spec/requests/requests/api/mendil/team_controller_spec.rb
+++ spec/requests/requests/api/mendil/team_controller_spec.rb
@@ -1,3 +1,13 @@
-RSpec.describe TeamController, type: :controller do
+RSpec.describe 'Team Controller', type: :request do
+  context 'GET team#index' do
+    it 'should return successful response' do
+      user = create :user, email:'test@test.com', password: '123456Aa!'
+      team = create :team, name:'perspolis', description:'666666', user_id: user.id
 
+      sign_in(user)
+
+      get '/api/mendil/team'
+      expect(response).to be_successful
+    end
+  end
 end
