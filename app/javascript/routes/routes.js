45b2f72b88dda1588a84e18a4ec155584b1caa12
khozi <khozaima.mahmoodi@gmail.com>
2019-12-17 11:42:01 +0330

    Add slide to show image

--- app/javascript/routes/routes.js
+++ app/javascript/routes/routes.js
@@ -1,10 +1,15 @@
 import navmenu from '../components/navmenu.vue'
+import showimg from '../components/showimg.vue'
 
 const routes = [
   {
     path: '/navmenu',
     name: 'navmenu',
     component: navmenu
+  },{
+    path: '/',
+    name: 'showimg',
+    component: showimg
   }
 ]
 
