54e3f0e356f810fa53d12966001c56aa81e76df3
khozi <khozaima.mahmoodi@gmail.com>
2019-12-19 23:32:42 +0330

    Edit image in lastteams components

--- app/javascript/components/lastteams.vue
+++ app/javascript/components/lastteams.vue
@@ -1,9 +1,10 @@
 <template>
 	<div id="app">
+    <hr>
     <el-row>
-      <el-col :span="5" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 0 : 0">
+      <el-col :span="6" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
         <el-card :body-style="{ padding: '0px' }">
-          <img src="https://www.realmadrid.com/img/ultrapanoramica_1500px/alegria-benzema-ramos-marcelo_fcl9659_ultra_20191218085708.jpg" class="image">
+          <img src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/12/19/15767600216146.jpg" class="image">
           <div style="padding: 14px;">
             <span>FOOTBALL</span>
             <div class="bottom clearfix">
@@ -14,9 +15,9 @@
         </el-card>
       </el-col>
 
-      <el-col :span="5" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 0 : 0">
+      <el-col :span="6" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
         <el-card :body-style="{ padding: '0px' }">
-          <img src="https://www.realmadrid.com/img/horizontal_940px/bestteamrandolph_940_20191203111042.jpg" class="image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1Qs1sBCuEUIH5JlfdtT-ljDc-_HB3EyZLAMD54G-GrKXmXdmiYQ&s" class="image">
           <div style="padding: 14px;">
             <span>BASKETBALL</span>
             <div class="bottom clearfix">
@@ -27,7 +28,7 @@
         </el-card>
       </el-col>
 
-      <el-col :span="5" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 0 : 0">
+      <el-col :span="6" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
         <el-card :body-style="{ padding: '0px' }">
           <img src="https://cdn.presstv.com/photo/20160816/636d5344-e566-4da6-8e46-c46b25ef2a0e.jpg" class="image">
           <div style="padding: 14px;">
@@ -40,7 +41,7 @@
         </el-card>
       </el-col>
 
-      <el-col :span="5" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 0 : 0">
+      <el-col :span="6" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
         <el-card :body-style="{ padding: '0px' }">
           <img src="https://www.gannett-cdn.com/-mm-/15558b270bca2dea9e41f0c562a7ff8ff3555c17/c=0-15-1022-592/local/-/media/2016/08/07/USATODAY/USATODAY/636061902018812550-handball-08-07.JPG?width=660&height=373&fit=crop&format=pjpg&auto=webp" class="image">
           <div style="padding: 14px;">
@@ -53,7 +54,7 @@
         </el-card>
       </el-col>
     </el-row>
-
+    <hr>
 
   </div>
 </template>
