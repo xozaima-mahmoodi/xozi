308226cba6fa8408cfda90f123863884dafd310a
khozi <khozaima.mahmoodi@gmail.com>
2019-12-22 16:00:10 +0330

    Edit showimg component

--- app/javascript/components/showimg.vue
+++ app/javascript/components/showimg.vue
@@ -1,10 +1,39 @@
 <template>
   <div id="app">
 
-    <el-carousel :interval="4000" type="card" height="500px">
-      <el-carousel-item v-for="item in 6" :key="item">
+    <el-carousel :interval="4000" type="card" height="400px">
+      <el-carousel-item v-for="item in 1" :key="item">
+        <img src="https://newsmedia.tasnimnews.com/Tasnim/Uploaded/Image/1398/07/07/1398070709312737818520494.jpg" class="image">
         <h3 class="medium">{{ item }}</h3>
       </el-carousel-item>
+
+      <el-carousel-item v-for="item in 1" :key="item">
+        <img src="https://cdn.presstv.com/photo/20160816/636d5344-e566-4da6-8e46-c46b25ef2a0e.jpg" class="image">
+        <h3 class="medium">{{ item }}</h3>
+      </el-carousel-item>
+
+      <el-carousel-item v-for="item in 1" :key="item">
+        <img src="https://media.gettyimages.com/photos/mohamed-salah-of-liverpool-celebrates-after-scoring-his-sides-first-picture-id944388156?s=612x612" class="image">
+        <h3 class="medium">{{ item }}</h3>
+      </el-carousel-item>
+
+      <el-carousel-item v-for="item in 1" :key="item">
+        <img src="https://sport.shafaqna.com/wp-content/uploads/2018/10/%D8%B1%D9%88%D9%86%D8%A7%D9%84%D8%AF%D9%88-2.jpg" class="image">
+        <h3 class="medium">{{ item }}</h3>
+      </el-carousel-item>
+
+      <el-carousel-item v-for="item in 1" :key="item">
+        <img src="https://newsmedia.tasnimnews.com/Tasnim/Uploaded/Image/1398/07/09/1398070909370691518541794.jpg" class="image">
+        <h3 class="medium">{{ item }}</h3>
+      </el-carousel-item>
+
+      <el-carousel-item v-for="item in 1" :key="item">
+        <img src="https://asansport.com/blog/wp-content/uploads/2019/05/basketball-900x600.jpg" class="image">
+        <h3 class="medium">{{ item }}</h3>
+      </el-carousel-item>
+
+
+
     </el-carousel>
     <Ls/>
   </div>
