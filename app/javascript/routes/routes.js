cebeeea84ce8a7f4783b708520956fd611e0fdc0
khozi <khozaima.mahmoodi@gmail.com>
2019-12-22 16:01:10 +0330

    Edit route in routes file

--- app/javascript/routes/routes.js
+++ app/javascript/routes/routes.js
@@ -1,6 +1,7 @@
 import Showimg from '../components/showimg.vue'
 import Lastteams from '../components/lastteams.vue'
-
+import Lastnews from '../components/lastnews.vue'
+import football from '../components/teams/football.vue'
 
 const routes = [
   {
@@ -11,6 +12,14 @@ const routes = [
     path: '/',
     name: 'lastteams',
     component: Lastteams
+  },{
+    path: '/',
+    name: 'lastnews',
+    component: Lastnews
+  },{
+    path: '/teams/football',
+    name: 'football',
+    component: football
   }
 ]
 
